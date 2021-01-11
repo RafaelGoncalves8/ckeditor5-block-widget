20fc9598aa2ca8f345a6c21562b3ccbd3888587b
Rafael Goncalves <rafaelgoncalves@riseup.net>
2021-01-11 12:16:29 -0300

    Done block widget tutorial

---
+++ simplebox/simpleboxui.js
@@ -0,0 +1,30 @@
+import ButtonView from "@ckeditor/ckeditor5-ui/src/button/buttonview";
+import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
+
+export default class SimpleBoxUI extends Plugin {
+  init() {
+    console.log("SimpleBoxUI#init() got called");
+
+    const editor = this.editor;
+    const t = editor.t;
+
+    editor.ui.componentFactory.add("simpleBox", (locale) => {
+      const command = editor.commands.get("insertSimpleBox");
+      const buttonView = new ButtonView(locale);
+
+      buttonView.set({
+        label: t("Simple box"),
+        withText: true,
+        tooltip: true,
+      });
+
+      buttonView.bind("isOn", "isEnabled").to(command, "value", "isEnabled");
+
+      this.listenTo(buttonView, "execute", () =>
+        editor.execute("insertSimpleBox")
+      );
+
+      return buttonView;
+    });
+  }
+}
